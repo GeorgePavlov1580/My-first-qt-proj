cbdbf30ff0ad734645233acff6433d8088a256e1
George <62718935+GeorgePavlov1580@users.noreply.github.com>
2022-01-17 19:31:58 +0300

    Update mainwindow.cpp

--- FirstProject/mainwindow.cpp
+++ FirstProject/mainwindow.cpp
@@ -13,16 +13,20 @@ MainWindow::~MainWindow(){
 }
 
 
+// моя ошибка, у тебя вообще этой функции быть не должно
 void MainWindow::on_pushButton_clicked(){
     // this does nothing, but if i remove it programm doesn't compile
 }
 
 void MainWindow::on_txt_btn_clicked(){
     ui -> chng_lbl -> setText(ui -> txt_entry -> text());
+    // ui -> id лэйбла, на котором меняяется текст -> функция изменения текста на лэйбле(ui -> id поля ввода -> текст на том самом поле)
+    // лэйбел с киберрсылкой (вообще 100% не обязательно)
     ui -> link_lbl -> setText("<a href=\"https://github.com/GeorgePavlov1580/My-first-qt-proj/\">My GitHub</a>");
     ui -> link_lbl -> setTextFormat(Qt::RichText);
     ui -> link_lbl -> setTextInteractionFlags(Qt::TextBrowserInteraction);
     ui -> link_lbl -> setOpenExternalLinks(true);
+    // конец
 
 }
 
